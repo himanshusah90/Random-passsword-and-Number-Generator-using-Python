030c04fcc7223a1e741500e8980f85a9af049614
HIMANSHU KUMAR SAH <141276345+himanshusah90@users.noreply.github.com>
2024-03-15 15:33:43 +0530

    Add files via upload

---
+++ Random Number Game.py
@@ -0,0 +1,19 @@
+import random
+
+target = random.randint(1, 100) 
+
+while True:
+    userChoice = (input("Guess the target or Quite : "))
+    if(userChoice == "Quite"):
+       break
+    userChoice = int(userChoice)
+    if(userChoice == target):
+      print("Sucess : correct Guess!!")
+      break
+    elif(userChoice < target):
+       print("your number was too small. Take a bigger guess..")
+    else:
+      print("your number was too big. Take a smaller guess..")
+
+      print("----Game over----")
+ 
